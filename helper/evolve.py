4af5c32f3687c74233b0b1e48e56b4c12bea1d50
Jesse Guenzl <contact@jesseguenzl.com>
2025-06-27 11:40:08 +0200

    save wip openEvolve code

---
+++ helper/evolve.py
@@ -0,0 +1,22 @@
+from openevolve import OpenEvolve
+import asyncio
+
+# Initialize the system
+evolve = OpenEvolve(
+    # initial_program_path="../train_ppo_sb3.py",
+    initial_program_path="../test.py",
+    evaluation_file="./evaluator.py",
+    config_path="./config.yaml"
+)
+
+
+async def main():
+    # Run the evolution
+    best_program = await evolve.run(iterations=10)
+    print(f"Best program metrics:")
+    for name, value in best_program.metrics.items():
+        print(f"  {name}: {value:.4f}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
