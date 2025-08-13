94946057201b5a31dc38cfc1d9b65502d4956f71
Jesse Guenzl <contact@jesseguenzl.com>
2025-08-13 12:15:49 +0200

    Revert "feat: switch from loading to creating Optuna study with maximize direction and load_if_exists option"
    This reverts commit 64e1b193630df1c0b79ca33d99c3eb336050dfe4.

--- src/train_ppo_sb3.py
+++ src/train_ppo_sb3.py
@@ -402,12 +402,10 @@ if __name__ == "__main__":
             url=args.storage,
         )
 
-        study = optuna.create_study(
+        study = optuna.load_study(
             study_name=args.study_name,
             storage=storage,
             pruner=optuna.pruners.MedianPruner(n_warmup_steps=10),
-            direction="maximize",
-            load_if_exists=True
         )
         mlflow_callback = MLflowCallback(
             tracking_uri=args.mlflow_tracking_uri,
